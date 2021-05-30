de139343d8726d1ba1121d10c40bae6c0ac2638a
JhojanHernandez <hernandez_1594@hotmail.com>
2021-05-30 10:58:11 -0500

    Se agrega el proyecto

---
+++ src/POO/Main.java
@@ -0,0 +1,18 @@
+package POO;
+
+public class Main {
+
+	public static void main(String[] args)
+	{
+		System.out.println("Inicio del programa");
+		
+		Persona hombre = new Persona();
+		Persona mujer = new Persona();
+  
+	}
+
+}
+
+/* Un objeto es una abstracción de la realidad */
+
+/* Una clase es la abstracción de ese objeto en un lenguaje de programación */
---
+++ src/POO/Persona.java
@@ -0,0 +1,29 @@
+package POO;
+
+public class Persona {
+	
+	// Atributos o caracteristicas
+	String nombre;
+	int edad;
+	int altura;
+	int peso;
+	
+	// Metodos o funciones
+	public void comer()
+	{
+		
+	}
+	
+	public void caminar()
+	{
+		
+	}
+	
+	public void dormir()
+	{
+		
+	}
+	
+}
+
+
